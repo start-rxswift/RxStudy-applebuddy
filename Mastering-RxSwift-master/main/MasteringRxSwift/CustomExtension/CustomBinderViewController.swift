4223eb55e43f934fef73d6ee946ad2434a18c110
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-12 19:02:13 +0900

    Study KXCoding-Lecture/Custom-Binder

--- Mastering-RxSwift-master/main/MasteringRxSwift/CustomExtension/CustomBinderViewController.swift
+++ Mastering-RxSwift-master/main/MasteringRxSwift/CustomExtension/CustomBinderViewController.swift
@@ -20,52 +20,79 @@
 //  THE SOFTWARE.
 //
 
+// CustomBinder는 어떻게 활용될 수 있을까요?
 import RxCocoa
 import RxSwift
 import UIKit
 
 class CustomBinderViewController: UIViewController {
     let bag = DisposeBag()
-
+    
     @IBOutlet var valueLabel: UILabel!
-
+    
     @IBOutlet var colorPicker: UISegmentedControl!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //        // #1) selectedSegmentIndex에서 방출하는 인덱스에 맞는 색상을 설정하고 Label의 텍스트 속성에 바인딩하고 있습니다.
+        //        colorPicker.rx.selectedSegmentIndex
+        //            .map { index -> UIColor in
+        //                switch index {
+        //                case 0:
+        //                    return UIColor.red
+        //                case 1:
+        //                    return UIColor.green
+        //                case 2:
+        //                    return UIColor.blue
+        //                default:
+        //                    return UIColor.black
+        //                }
+        //            }
+        //            .subscribe(onNext: { [weak self] color in
+        //                self?.valueLabel.textColor = color
+        //            })
+        //            .disposed(by: bag)
+        //
+        //        colorPicker.rx.selectedSegmentIndex
+        //            .map { index -> String? in
+        //                switch index {
+        //                case 0:
+        //                    return "Red"
+        //                case 1:
+        //                    return "Green"
+        //                case 2:
+        //                    return "Blue"
+        //                default:
+        //                    return "Unknown"
+        //                }
+        //            }
+        //            .bind(to: valueLabel.rx.text)
+        //            .disposed(by: bag)
         colorPicker.rx.selectedSegmentIndex
-            .map { index -> UIColor in
-                switch index {
-                case 0:
-                    return UIColor.red
-                case 1:
-                    return UIColor.green
-                case 2:
-                    return UIColor.blue
-                default:
-                    return UIColor.black
-                }
-            }
-            .subscribe(onNext: { [weak self] color in
-                self?.valueLabel.textColor = color
-            })
+            .bind(to: valueLabel.rx.segmentedValue)
             .disposed(by: bag)
+    }
+}
 
-        colorPicker.rx.selectedSegmentIndex
-            .map { index -> String? in
-                switch index {
-                case 0:
-                    return "Red"
-                case 1:
-                    return "Green"
-                case 2:
-                    return "Blue"
-                default:
-                    return "Unknown"
-                }
+// #2) CustomBinder를 통해 더욱더 간결하게 코드를 구현할 수 있습니다.
+extension Reactive where Base: UILabel {
+    var segmentedValue: Binder<Int> {
+        return Binder(self.base) { label, index in
+            switch index {
+            case 0:
+                label.text = "Red"
+                label.textColor = UIColor.red
+            case 1:
+                label.text = "Green"
+                label.textColor = UIColor.green
+            case 2:
+                label.text = "Blue"
+                label.textColor = .blue
+            default:
+                label.text = "Unknown"
+                label.textColor = .black
             }
-            .bind(to: valueLabel.rx.text)
-            .disposed(by: bag)
+        }
     }
 }
