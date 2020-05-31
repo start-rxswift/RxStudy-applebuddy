916c92ec6430f61933e1e3949203b3b9c3937651
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-01 00:24:50 +0900

    Study KXCoding-Rx-Lecture/timerOperator

--- Mastering-RxSwift-master/main/Operators/Time-based Operators/timer.playground/Contents.swift
+++ Mastering-RxSwift-master/main/Operators/Time-based Operators/timer.playground/Contents.swift
@@ -20,6 +20,11 @@
 //  THE SOFTWARE.
 //
 
+// MARK: - timer
+
+// - timer 연산자는 interval 과 마찬가지로 정수를 반복적으로 방출하는 Observable을 생산합니다.
+// - 히지만 지연방식과 반복주기를 동시에 설정할 수 있고, 두 값에 따라 동작방식이 달라집니다.
+
 import RxSwift
 import UIKit
 
@@ -28,3 +33,21 @@ import UIKit
  */
 
 let bag = DisposeBag()
+
+// MARK: parameter
+
+// - dueTime: RxTimeInterval : 첫번째 요소가 방출되는 시점까지의 상대적 시간, 구독을 시작하고 첫번째 - 요소가 구독자에게 전달되는 시간, 아래의 경우 첫번째 요소가 구독 후 1초 뒤에 전달된다.
+// - period: RxTimeInterval? = nil : 반복주기
+// - scheduler : 타이머를 전달할 스케쥴러
+
+// 기본 타이머 실행 예시
+// Observable<Int>.timer(.seconds(1), scheduler: MainScheduler.instance)
+//    .subscribe { print($0) }
+//    .disposed(by: bag)
+
+// * period를 .milliseconds(500)으로 설정하면 1초 마다 타이머가 실행됩니다.
+Observable<Int>.timer(.seconds(1),
+                      period: .milliseconds(1000),
+                      scheduler: MainScheduler.instance)
+    .subscribe { print($0) }
+    .disposed(by: bag)
