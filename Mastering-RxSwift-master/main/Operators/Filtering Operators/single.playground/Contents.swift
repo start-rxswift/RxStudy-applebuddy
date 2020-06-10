e05cc37e1abc3e8408eceb51457a409157dff18c
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-10 20:24:14 +0900

    Study KXCoding-Rx-Lecture/Operator_single

--- Mastering-RxSwift-master/main/Operators/Filtering Operators/single.playground/Contents.swift
+++ Mastering-RxSwift-master/main/Operators/Filtering Operators/single.playground/Contents.swift
@@ -20,6 +20,10 @@
 //  THE SOFTWARE.
 //
 
+// MARK: - single operator
+
+// - 요소 중 맨 처음 하나의 요소만 방출합니다.
+
 import RxSwift
 import UIKit
 
@@ -29,3 +33,37 @@ import UIKit
 
 let disposeBag = DisposeBag()
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// result) next(1), completed
+Observable.just(1)
+    .single()
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+// - single() 연산자를 사용할 때는 단 하나의 요소만 전달되야 합니다. 원본 옵저버블이 0, 2~ 개의 값을 방출하면 안됩니다.
+// result) next(1)이 방출 되지만, error 이벤트가 발생합니다.(Sequence contains more than one element.)
+Observable.from(numbers)
+    .single()
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+// - single 연산자가 단 하나의 요소만 전달할 수 있도록 predicate 클로져 조건을 두었을때 단 하나의 요소만 충족된다면 성립되어 단 하나의 요소를 전달할 수 있습니다.
+Observable.from(numbers)
+    .single { $0 == 6 }
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+let subject = PublishSubject<Int>()
+subject.single()
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
+
+// - 하나의 Int 요소를 subject에서 방출하면 바로 구독자에게 해당 요소가 전달됩니다. single 연산자는 subject가 completed 이벤트가 발생하기 전까지 단 하나의 요소만 방출하지 않는다면 error 이벤트를 방출하게 됩니다.
+// - 이런 single 연산자의 특성으로 하나의 요소만 방출되는 것을 보장할 수 있습니다.
+subject.onNext(100)
+
+// - 해당 시점에 onCompleted 이벤트를 전달받으면, 이전까지 방출된 요소는 단 하나이므로 정상적으로 completed 이벤트를 구독자에게 전달합니다.
+subject.onCompleted()
+
+// - completed 이벤트 이전에 두개 이상의 요소가 방출되었으므로, error이벤트가 발생합니다.
+// subject.onNext(99)
